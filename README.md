c172e42a7f404d3a1457ae8b8329f264f93e4469
Jakob Sommerhuber <jakob@sommerhuber.name>
2025-09-21 15:57:30 +0200

    Add readme and tests

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# amis-reader
+# AMIS Reader
+
+This project provides a set of Ruby scripts to read data from an AMIS (Advanced Metering Infrastructure System) smart meter, process it, and store it in CSV files.
+
+## How it Works
+
+The project consists of two main components:
+
+*   `amis_reader_reader.rb`: A script that fetches JSON data from a smart meter at a specified URL (`http://10.0.0.99/rest` by default). It then transforms the data and appends it to a daily CSV file (e.g., `amis_reader_2025-09-21.csv`).
+*   `amis_reader_scheduler.rb`: A script that uses the `rufus-scheduler` gem to run the `amis_reader_reader.rb` script at a regular interval (every second by default).
+
+## Requirements
+
+*   Ruby
+*   Bundler
+
+## Installation
+
+1.  Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/amis-reader.git
+    cd amis-reader
+    ```
+
+2.  Install the required gems using Bundler:
+
+    ```bash
+    bundle install
+    ```
+
+## Usage
+
+To start the data collection process, run the scheduler:
+
+```bash
+bundle exec ruby amis_reader_scheduler.rb
+```
+
+This will start a background process that reads data from the smart meter every second and saves it to a new CSV file each day.
+
+## Testing
+
+To run the RSpec tests, use the following command:
+
+```bash
+bundle exec rspec
+```
+
+## Configuration
+
+The URL of the smart meter can be configured by changing the `URL` constant in the `amis_reader_reader.rb` file.
---
+++ spec/amis_reader_reader_spec.rb
@@ -0,0 +1,64 @@
+require_relative 'spec_helper'
+
+describe AmisReaderReader do
+  let(:reader) { AmisReaderReader.new }
+  let(:url) { 'http://example.com' }
+  let(:json_response) do
+    '{ "time": 1678886400, "1.8.0": 12345.6, "2.8.0": 6789.0 }'
+  end
+  let(:data) { JSON.parse(json_response) }
+  let(:date) { Time.at(data['time']).utc.to_date }
+  let(:filename) { "amis_reader_#{date.strftime('%Y-%m-%d')}.csv" }
+  let(:values) { ["2023-03-15 13:20:00", 12.345600000000001, 6.789] }
+
+  before do
+    allow(Net::HTTP).to receive(:get).with(URI(url)).and_return(json_response)
+    allow(CSV).to receive(:open).with(any_args)
+  end
+
+  describe '#run' do
+    it 'fetches JSON, transforms it, and appends it to a CSV' do
+      expect(reader).to receive(:fetch_json_from_url).with(url).and_return(data)
+      expect(reader).to receive(:transform_data).with(data).and_return(values)
+      expect(reader).to receive(:append_values_to_csv).with(filename, values)
+      reader.run(url)
+    end
+  end
+
+  describe '#fetch_json_from_url' do
+    it 'returns a parsed JSON object' do
+      expect(reader.fetch_json_from_url(url)).to eq(data)
+    end
+
+    it 'handles errors and returns an empty hash' do
+      allow(Net::HTTP).to receive(:get).with(URI(url)).and_raise(StandardError)
+      expect(reader.fetch_json_from_url(url)).to eq({})
+    end
+  end
+
+  describe '#build_filename' do
+    it 'returns a correctly formatted filename' do
+      expect(reader.build_filename(date)).to eq(filename)
+    end
+  end
+
+  describe '#transform_data' do
+    it 'transforms the data correctly' do
+      expect(reader.transform_data(data)).to eq(values)
+    end
+  end
+
+  describe '#append_values_to_csv' do
+    it 'appends values to a CSV file' do
+      expect(CSV).to receive(:open).with(filename, 'a')
+      reader.append_values_to_csv(filename, values)
+    end
+
+    it 'creates a new file with headers if it does not exist' do
+      allow(File).to receive(:exist?).with(filename).and_return(false)
+      expect(CSV).to receive(:open).with(filename, 'w')
+      expect(CSV).to receive(:open).with(filename, 'a')
+      reader.append_values_to_csv(filename, values)
+    end
+  end
+end
