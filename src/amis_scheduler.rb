f90650092f860f7ae8956055a19343310e0a97c4
Christian Sommerhuber <sommerhuber@hotmail.com>
2025-09-25 17:37:09 +0200

    added rescue to make it failure save and gets in case of error to save the message for reading

--- src/amis_scheduler.rb
+++ src/amis_scheduler.rb
@@ -2,14 +2,19 @@ ENV['TZ'] = 'Europe/Vienna'
 require 'rufus-scheduler'
 require './src/amis_reader'
 
-amis=AmisReader.new
-scheduler = Rufus::Scheduler.new
-job_id = scheduler.every '1s' do
-  amis.run
-  if File.exist?("C:/temp/end")
-    scheduler.unschedule(job_id)
+begin
+  amis=AmisReader.new
+  scheduler = Rufus::Scheduler.new
+  job_id = scheduler.every '1s' do
+    amis.run
+    if File.exist?("C:/temp/end")
+      scheduler.unschedule(job_id)
+    end
   end
+  scheduler.join
+  File.unlink("C:/temp/end")
+rescue
+  File.unlink("C:/temp/end")
+  print "finish by inputting some character(s): "
+  gets
 end
-scheduler.join
-puts "DONE"
-File.unlink("C:/temp/end")
